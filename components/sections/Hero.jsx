d535ef30d568715ab24df05910d2164cd21bbeb9
Developer Junaid <qjunaid623@gmail.com>
2022-10-27 22:51:02 +0500

    simplified ui like shoppable campaign and connected with campaign section of sanity

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -9,10 +9,19 @@ import { ProductContext } from '../../context/ProductContext'
 export const Hero = () => {
 
     const { allData } = useContext(ProductContext)
+    console.log(allData?.image?.url)
 
     return (
-        <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
-            <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+        <div >
+            <div className=" inset-0 overflow-hidden">
+                {allData && allData?.image && <img
+                    src={allData?.image?.url}
+                    alt={"okay"}
+                    className=" h-full w-full object-cover object-center"
+                />}
+
+            </div>
+            <div className="pt-8 pb-80 sm:pt-24 sm:pb-40 lg:pt-10 lg:pb-48"><div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8" >
                 <div className="sm:max-w-lg">
                     {allData && allData?.headline && <h1 className="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                         {allData?.headline}
@@ -24,97 +33,18 @@ export const Hero = () => {
                 </div>
                 <div>
                     <div className="mt-10">
-                        {/* Decorative image grid */}
-                        <div
-                            aria-hidden="true"
-                            className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-                        >
-                            <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                                <div className="flex items-center space-x-6 lg:space-x-8">
-                                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                                        {allData?.images && allData?.images[0] && <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                                            <img
-                                                src={allData?.images[0]?.url}
-                                                alt={allData?.images[0]?.name}
-                                                className="h-full w-full object-cover object-center"
-                                            />
-                                        </div>}
-                                        {allData?.images && allData?.images[1] && <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                            <img
-                                                src={allData?.images[1]?.url}
-                                                alt={allData?.images[1]?.name}
-                                                className="h-full w-full object-cover object-center"
-                                            />
-                                        </div>}
-                                    </div>
-                                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-
-                                        {allData?.images && allData?.images[2] && <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                            <img
-                                                src={allData?.images[2]?.url}
-                                                alt={allData?.images[2]?.name}
-                                                className="h-full w-full object-cover object-center"
-                                            />
-                                        </div>}
-
-
-
-
-                                        {allData?.images && allData?.images[3] && <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                            <img
-                                                src={allData?.images[3]?.url}
-                                                alt={allData?.images[3]?.name}
-                                                className="h-full w-full object-cover object-center"
-                                            />
-                                        </div>}
-
-
-
-                                        {allData?.images && allData?.images[4] && <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                            <img
-                                                src={allData?.images[4]?.url}
-                                                alt={allData?.images[4]?.name}
-                                                className="h-full w-full object-cover object-center"
-                                            />
-                                        </div>}
-
-
-
-
-                                    </div>
-                                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-
-                                        {allData?.images && allData?.images[5] && <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                            <img
-                                                src={allData?.images[5]?.url}
-                                                alt={allData?.images[5]?.name}
-                                                className="h-full w-full object-cover object-center"
-                                            />
-                                        </div>}
-
-
-                                        {allData?.images && allData?.images[6] && <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                            <img
-                                                src={allData?.images[6]?.url}
-                                                alt={allData?.images[6]?.name}
-                                                className="h-full w-full object-cover object-center"
-                                            />
-                                        </div>}
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         <Link
-                            href="/collection"
+                            href="#_"
 
                         >
-                            <span className="inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700 cursor-pointer">                                    Shop Collection
+                            <span className="inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700 cursor-pointer">                                    SHOP LOOK
                             </span>
                         </Link>
                     </div>
                 </div>
-            </div>
+            </div></div>
+
+
+
         </div>)
 }
--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,7 @@ import { client } from "../lib/client"
 
 // Components
 import { Hero } from "../components/sections/Hero"
-import { Category } from "../components/sections/Category"
-import { Featured } from "../components/sections/Featured"
 import { Favourites } from "../components/sections/Favourites"
-import { CTA } from "../components/sections/CTA"
 import { Layout } from "../components/Layout"
 
 export default function Home({ data }) {
@@ -37,141 +34,29 @@ export default function Home({ data }) {
         {/* Hero section */}
         <Hero />
       </div>
-
-      {/* Category section */}
-      <Category />
-
-      {/* Featured section */}
-      <Featured />
-
-      {/* Favorites section */}
-      <Favourites />
-
-      {/* CTA section */}
-      <CTA />
     </Layout>
   )
 }
 
 export async function getServerSideProps() {
-  const query = `*[_type == "head"]{
+  const query = `*[_type == "campaign"]{
     id,
-    name,
-    country,
-    flag{
-      'url': asset->url
-    },
+    name, 
     headline,
-    subHeading,
-    'images': images[]->{
-      name,
-      'url': images.asset->url
-    },
-    url,
-    bannerHeading,
-    bannerText,
-    saleText,
-    bannerImage{
+    subHeading, 
+    image{
       'url': asset->url
     },
-    'categories': categories[]->{
-      name,
-      label,
-      slug,
-      description,
-      image{
-        'url': asset->url
-      },
-    'subCategories': subCategories[]->{
-      name,
-      label,
-      slug,
-      description,
-      image{
-      'url': asset->url
-    },
-      'products': products[]->{
-        name,
-        description,
-        caption,
-        'colors': colors[]->{
-          name,
-          code,
-        },         
-        'sizes': sizes[]->{
-          name,
-        },
-        reference,
-        'images': images[]->{
-          name,
-          description,
-          'url': images.asset->url
-      }
-    }}},   
-    'favourites':favourites[]->{
-      name,
-        description,
-        caption,
-        reference,
-        'colors': colors[]->{
-          name,
-          code,
-        },         
-        'sizes': sizes[]->{
-          name,
-        },
-        'images': images[]->{
-          name,
-          description,
-          'url': images.asset->url
-      }
-    },
-    'sale':sale[]->{
-      name,
-        description,
-        caption,
-        reference,
-        'colors': colors[]->{
-          name,
-          code,
-        },         
-        'sizes': sizes[]->{
-          name,
-        },
-        'images': images[]->{
-          name,
-          description,
-          'url': images.asset->url
-      }
-    },
-    'collection':collection[]->{
-      name,
-        description,
-        caption,
-        reference,
-        'colors': colors[]->{
-          name,
-          code,
-        },         
-        'sizes': sizes[]->{
-          name,
-        },
-        'images': images[]->{
-          name,
-          description,
-          'url': images.asset->url
-      }
-    }
 }`
 
   // Get Sanity Data
-  const heads = await client.fetch(query)
+  const campaigns = await client.fetch(query)
 
   let filtered = {}
 
-  if (heads.length > 0) {
-    filtered = heads.filter(
-      (head) => head.id === process.env.NEXT_PUBLIC_CAMPAIGN_ID
+  if (campaigns.length > 0) {
+    filtered = campaigns.filter(
+      (campaign) => campaign.id === process.env.NEXT_PUBLIC_CAMPAIGN_ID
     )
   }
 
